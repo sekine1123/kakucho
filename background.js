d4046e4ef297b80e147bf460152ea12d35606eb5
sekine1123 <kankon1123@yahoo.co.jp>
2024-09-02 11:59:29 +0900

    監視対象サイトかどうかの判別が正しくできるようになった！これからcheckTIme関数的なのを作り、制限時間・クールタイムの確認する

--- background.js
+++ background.js
@@ -13,14 +13,12 @@ const Msites = [
 ];
 
 
-
-//このバージョンだと、youtubeにアクセスしても制限サイトとして認識されない
-
+//監視対象サイトかどうかの判別が正しくできるようになった！これからcheckTIme関数的なのを作り、制限時間・クールタイムの確認する
 
 
 //以下コード
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//タブが更新された2秒後に動くコード
+//タブが更新された5秒後に動くコード
     console.log("タブ更新",flag_geturl)
 
     //URL取得ができていなかった場合のみ動作
@@ -32,14 +30,15 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
                 console.log("URLの取得成功:",changeInfo.url)
                 flag_geturl = true;
 
-                const isMsite = Msites.some(site => changeInfo.url.includes(site));
+                const isMsite = Msites.some(site => new RegExp(site.replace(/\*/g, '.*')).test(changeInfo.url));
                 if (isMsite) {
-                    console.log("制限サイト内",changeInfo.url);
+                    console.log("制限サイト内,checktimeへ:",changeInfo.url);
                 } else {
                     console.log("制限リスト外",changeInfo.url);
+                    flag_geturl = false;
                 }
 
-            //URLを取得失敗またはundefinedであった場合は2秒待機して再試行
+            //URLを取得失敗またはundefinedであった場合は5秒待機して再試行
             }else{
                 console.log("URLの取得失敗、再試行:",changeInfo.url)
 
@@ -48,20 +47,21 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
                         if (updatedTab.url||!updatedTab.url==undefined) {
                             console.log("2度目にしてURLの取得成功:",updatedTab.url);
                             flag_geturl = true;
-                            const isMsite = Msites.some(site => updatedTab.url.includes(site));
+                            const isMsite = Msites.some(site => new RegExp(site.replace(/\*/g, '.*')).test(updatedTab.url));
                             if (isMsite) {
-                                console.log("制限サイト内");
+                                console.log("制限サイト内,checktimeへ:");
                             } else {
                                 console.log("制限リスト外");
+                                flag_geturl = false;
                             }
                         } else {
                             console.log("2度目もURLの取得失敗",changeInfo.url);
                         }
                     });
-                }, 2000);
+                }, 5000);
 
             }
-        },2000) 
+        },5000) 
     }
 })
 
